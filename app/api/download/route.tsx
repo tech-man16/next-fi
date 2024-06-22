9fbd8631c260017216a59dac6ff27e7ea7bfb45d
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-22 16:14:24 +0530

    Update route.tsx

--- app/api/download/route.tsx
+++ app/api/download/route.tsx
@@ -4,25 +4,27 @@ import { readFileSync, readdirSync } from 'fs';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req:NextRequest) {
-  return NextResponse.json({message:"Successfull"}) ;
-  /*
-  const url = new URL(req.url)
-  const filename = url.searchParams.get("file") ;
-  const file = readFileSync(`./next-fi/app/api/assets/${filename}`);
-  const headers = new Headers();
-  headers.append('Content-Disposition', `attachment; filename="${filename}"`);
-  headers.append('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-  headers.append('Content-Length', `${file.length}`)
-  headers.append('Content-Transfer-Encoding', 'binary')
-
-  return new Response(file, {
-    headers,
-  });
-  */
+  try{
+    const url = new URL(req.url)
+    const filename = url.searchParams.get("file") ;
+    const file = readFileSync(`./app/api/assets/${filename}`);
+    const headers = new Headers();
+    headers.append('Content-Disposition', `attachment; filename="${filename}"`);
+    headers.append('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+    headers.append('Content-Length', `${file.length}`)
+    headers.append('Content-Transfer-Encoding', 'binary')
+  
+    return new Response(file, {
+      headers,
+    });
+  }
+  catch(e){
+    return NextResponse.json({message:"Failed"}) ;
+  }
 }
 
 export async function POST(req: any, res: any) {
-  const file = readdirSync(process.cwd() + '/next-fi/app/api/assets')
+  const file = readdirSync(process.cwd() + './app/api/assets')
 
   return NextResponse.json({ message: file });
 }
