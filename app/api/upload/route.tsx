d4c51b47f14bff221df8e67ebbe0b5e97c233829
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-22 16:16:30 +0530

    Update route.tsx

--- app/api/upload/route.tsx
+++ app/api/upload/route.tsx
@@ -4,22 +4,26 @@ import { writeFile,readdir } from "fs/promises";
 
 export const POST = async (req: any, res: any) => {
 
-    const formData = await req.formData();
-    const file = formData.get("file");
-    if (!file) {
-        // If no file is received, return a JSON response with an error and a 400 status code
-        return NextResponse.json({ error: "No files received." }, { status: 400 });
+    try{
+        const formData = await req.formData();
+        const file = formData.get("file");
+        if (!file) {
+            // If no file is received, return a JSON response with an error and a 400 status code
+            return NextResponse.json({ error: "No files received." }, { status: 400 });
+        }
+        const buffer = Buffer.from(await file.arrayBuffer());
+    
+        try {
+            await writeFile(path.join(process.cwd(), 'next-fi/app/api/assets/', file.name), buffer);
+            return NextResponse.json({ message: 'Uploaded Successfully', status: 200 }, { status: 200 })
+        }
+        catch (e) {
+            console.log(process.cwd(), '/assets/', file.name);
+    
+            console.log('\n\n\n', e)
+            return NextResponse.json({ message: 'Uploaded Failed', status: 500 }, { status: 500 })
+        }
     }
-    const buffer = Buffer.from(await file.arrayBuffer());
 
-    try {
-        await writeFile(path.join(process.cwd(), 'next-fi/app/api/assets/', file.name), buffer);
-        return NextResponse.json({ message: 'Uploaded Successfully', status: 200 }, { status: 200 })
-    }
-    catch (e) {
-        console.log(process.cwd(), '/assets/', file.name);
-
-        console.log('\n\n\n', e)
-        return NextResponse.json({ message: 'Uploaded Failed', status: 500 }, { status: 500 })
-    }
+    catch(e) {return NextResponse.json("message":"Loading...") };
 }
