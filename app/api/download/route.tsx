15c56661a70ca8e1ce2799a38140a2cf03bb7783
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-22 21:28:23 +0530

    Update route.tsx

--- app/api/download/route.tsx
+++ app/api/download/route.tsx
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import path from "path";
 import { writeFile,readdir } from "fs/promises";
 
-export async function POST (req: any, res: any) => {
+export async function POST (req: any, res: any) {
   return NextResponse.json({message:"Success",status:200},{status:200})
 }
